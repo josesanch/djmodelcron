62445c25ac942baae18c968316551da32ea4aafa
José Sánchez Moreno <jose@o2w.es>
2015-07-14 12:51:50 +0200

    Include celery task

--- djmodelcron/tasks.py
+++ djmodelcron/tasks.py
@@ -1,25 +1,22 @@
 from django.utils import timezone
 
-from celery_app import app
-from erpnotify.models import Tarea
+from celery.Celery import task
 
 from .models import Cron
 
 
-@app.task()
-def run_tarea(tarea_id):
-    tarea = Tarea.objects.get(pk=tarea_id)
-    tarea.run()
+@task()
+def run_task(content_type, pk):
+    cron = Cron.objects.get(pk=pk)
+    cron.content_object.run()
 
 
-@app.task(name="cron")
+@task(name="cron")
 def cron():
     now = timezone.now().replace(second=0, microsecond=0)
-    print "* CRON {}".format(now)
     crones = Cron.objects.filter(enabled=True)
-    print crones
+
     for cron in crones:
         ocurrences = cron.in_minutes(now)
         for ocurrence in ocurrences:
-            print cron.content_object
-            run_tarea.apply_async([cron.content_object.pk], eta=ocurrence)
+            run_task.apply_async([cron.pk], eta=ocurrence)
