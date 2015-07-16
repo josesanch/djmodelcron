40f26b2446c4956f77abe0c985cf5ab945daf4c7
José Sánchez Moreno <jose@o2w.es>
2015-07-16 16:44:54 +0200

    Migrations

---
+++ djmodelcron/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import recurrence.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Cron',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('enabled', models.BooleanField(default=True)),
+                ('start', models.DateTimeField()),
+                ('recurrence', recurrence.fields.RecurrenceField()),
+                ('object_id', models.PositiveIntegerField(null=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', null=True)),
+            ],
+        ),
+    ]
